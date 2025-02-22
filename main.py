c602c1a0f512953543ba2678afd69699419c9ed6
5H4D0WM0535 <padraigbac@proton.me>
2025-02-22 15:05:25 +0000

    Added FPS to the game

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +19,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(FPS)
 
 if __name__ == "__main__":
     main()
