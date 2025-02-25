e187cadfd2a874406d48101e0c4b4cc557360956
5H4D0WM0535 <padraigbac@proton.me>
2025-02-25 18:08:09 +0000

    Added shot class and shooting to the player

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
 from player import Player
 from asteroid import Asteroid
 from asteroids import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,9 +13,11 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updateable, drawable)
     Asteroid.containers = (asteroids, updateable, drawable)
-    AsteroidField.containers = (updateable)
+    AsteroidField.containers = (updateable,)
+    Shot.containers = (shots, updateable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -33,6 +36,11 @@ def main():
                 return
         
         updateable.update(dt)
+        shots.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over!")
+                return
         screen.fill("black")
         for img in drawable:
             img.draw(screen)
